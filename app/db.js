372bd0376d0bb6509b551eafe646eb6c73b1efe6
alemalagni <alemalagni@gmail.com>
2025-05-14 11:15:50 +0200

    feat: add error catch

--- app/db.js
+++ app/db.js
@@ -6,3 +6,13 @@ const connection = mysql.createConnection({
     password: 'Sedmo1-fyqtyw-tehboc',
     database: 'movies_db'
 });
+
+connection.connect((err) => {
+    if (err) {
+        console.log('Errore di connessione con il database', err);
+        return;
+    }
+    console.log('Connesso al database!');
+});
+
+module.exports = connection;

0512f667e32ebe1bfee66ee472df0528b86b8e18
alemalagni <alemalagni@gmail.com>
2025-05-14 11:07:34 +0200

    feat: add connection

--- app/db.js
+++ app/db.js
@@ -1,1 +1,8 @@
 const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Sedmo1-fyqtyw-tehboc',
+    database: 'movies_db'
+});
